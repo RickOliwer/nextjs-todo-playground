075857fe302e7c93b1e5a7843e26b8fb8df4c528
RickOliwer <rickoliwer@gmail.com>
2025-07-05 17:09:43 +0200

    feat: create a data access layer (DAL)

---
+++ src/lib/dal.ts
@@ -0,0 +1,34 @@
+import "server-only";
+import { cache } from "react";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/server/db";
+import { account } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+
+export const verifySession = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const existingUser = await db.query.account.findFirst({
+    where: eq(account.clerkId, userId),
+    columns: { id: true, clerkId: true, email: true },
+  });
+
+  if (!existingUser) {
+    await db
+      .insert(account)
+      .values({
+        clerkId: userId,
+        email: "",
+      })
+      .onConflictDoNothing();
+  }
+
+  return {
+    isAuth: true,
+    clerkId: userId,
+  };
+});
