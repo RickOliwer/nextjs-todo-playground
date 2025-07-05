d3b719921a3be83314b3fb907e9eb4f8b9d46c10
RickOliwer <rickoliwer@gmail.com>
2025-07-05 12:26:22 +0200

    update: readme

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -12,16 +12,30 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `todo-app_${name}`);
 
-export const posts = createTable(
-  "post",
+export const todos = createTable(
+  "todos",
   (d) => ({
     id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
+    title: d.varchar({ length: 256 }),
+    description: d.varchar({ length: 256 }),
+    completed: d.boolean().default(false),
     createdAt: d
       .timestamp({ withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
   }),
-  (t) => [index("name_idx").on(t.name)],
+  (t) => [index("title_idx").on(t.title)],
 );
+
+export const account = createTable("account", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  name: d.varchar({ length: 256 }),
+  email: d.varchar({ length: 256 }),
+  clerkId: d.varchar({ length: 256 }),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+}));
